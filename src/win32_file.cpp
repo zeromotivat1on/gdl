de65c8705126b877d7155969c0afcf4e6e3f4ed4
Alexander <lyannoy.alexander@gmail.com>
2024-11-10 19:56:26 +0100

    file open close win32

--- src/win32_file.cpp
+++ src/win32_file.cpp
@@ -9,14 +9,14 @@ s32 FILE_ACCESS_EXEC    = GENERIC_EXECUTE;
 s32 FILE_CREATE_NEW     = CREATE_NEW;
 s32 FILE_OPEN_EXISTING  = OPEN_EXISTING;
 
-fhandle fstart(const char* fpath, s32 access, s32 start)
+fhandle fopen(const char* fpath, s32 access, s32 start)
 {
     wchar_t wfpath[512];
     utf8to16(fpath, wfpath);
     return CreateFileW(wfpath, access, 0, NULL, start, FILE_ATTRIBUTE_NORMAL, NULL);
 }
 
-bool fstop(fhandle handle)
+bool fclose(fhandle handle)
 {
     return CloseHandle(handle);
 }
