61b9985de9c760a2873d3360b7adbfff9e929071
Alexander <lyannoy.alexander@gmail.com>
2024-11-27 23:53:41 +0100

    remove comment, fix macro tab

--- src/ecs.h
+++ src/ecs.h
@@ -22,11 +22,6 @@ struct ECS
     u32         free_entity_count;
 };
 
-// NOTE: Maybe store components in hash tables with keys as string == component type name?
-// ecs_component_register(ecs, sid(component_name), component_size);
-//#define ecs_component_struct(ecs, e, type) (type*)ecs_component_get(ecs, e, MACRO_STRING(type));
-//CameraComponent/auto* camera = ecs_component_struct(ecs, entity, CameraComponent);
-
 void    ecs_init(ECS* ecs, Arena* arena, u32 max_entities, u16 max_component_type_count);
 Entity  ecs_entity_new(ECS* ecs);
 void    ecs_entity_del(ECS* ecs, Entity e);
@@ -36,6 +31,6 @@ bool    ecs_component_add(ECS* ecs, Entity e, sid ct);
 void*   ecs_component_get(ECS* ecs, Entity e, sid ct);
 bool    ecs_component_del(ECS* ecs, Entity e, sid ct);
 
-#define ecs_component_reg_struct(ecs, arena, ct) ecs_component_reg(ecs, arena, SID(MACRO_STRING(ct)), sizeof(ct))
-#define ecs_component_add_struct(ecs, e, ct)    ecs_component_add(ecs, e, SID(MACRO_STRING(ct)))
-#define ecs_component_get_struct(ecs, e, ct)    (ct*)ecs_component_get(ecs, e, SID(MACRO_STRING(ct)))
+#define ecs_component_reg_struct(ecs, arena, ct)    ecs_component_reg(ecs, arena, SID(MACRO_STRING(ct)), sizeof(ct))
+#define ecs_component_add_struct(ecs, e, ct)        ecs_component_add(ecs, e, SID(MACRO_STRING(ct)))
+#define ecs_component_get_struct(ecs, e, ct)        (ct*)ecs_component_get(ecs, e, SID(MACRO_STRING(ct)))
